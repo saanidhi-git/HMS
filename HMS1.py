7661fb8eec6a8f5e7f958c2f657d8dbf14f88f10
Sahil Katurde <sahilkaturde1@gmail.com>
2025-03-22 04:00:38 -0700

    Created some changes such as I deleted extra folders

--- HMS1.py
+++ HMS1.py
@@ -1,4 +1,19 @@
+from HMS.data import *  # Ensure maincourse and beverages are defined in data.py
+
 class Menu:
-    def __init__(self,maincourse,beverages):
-        self.maincourse=maincourse
-        self.beverages=beverages
+    def __init__(self, maincourse, beverages):
+        self.maincourse = maincourse
+        self.beverages = beverages
+        self.menu_print()
+
+    def menu_print(self):
+        # Use self.maincourse and self.beverages
+        food_categories = [self.maincourse, self.beverages]
+        for category in food_categories:
+            for food, price in category.items():
+                print(f"{food}: {price}Rs")
+
+
+# Create an instance of the Menu class
+if __name__ == "__main__":
+    menu = Menu(maincourse, beverages)
